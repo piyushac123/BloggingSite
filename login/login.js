f4b9c1ad0e67104c2c5c0eedc0351f93ecc9488b
piyushac123 <piyush.chincholikar@gmail.com>
2019-01-28 23:27:00 +0530

    Fourth update

--- login/login.js
+++ login/login.js
@@ -1,3 +1,55 @@
 function func(){
     window.location.replace("../blog1/blog1.xml");
 }
+function func1()								 
+{ 		 
+	var email = document.forms["RegForm"]["EMail"]; 
+	var password = document.forms["RegForm"]["Password"];
+	var value1="";
+	var flag=0;
+	if(email.value!=""&&password.value!=""){
+		if(email.value!="piyush@gmail.com"||password.value!="piyush"){
+			value1+="Invalid Email or password\n"; 
+			flag=1;
+		}
+	}
+	else{
+		if (email.value == "")								 
+		{ 
+			value1+="Please enter a valid e-mail address.\n"; 
+			email.focus(); 
+			flag=1;
+		} 
+
+		else if (email.value.indexOf("@", 0) < 0)				 
+		{ 
+			value1+="Please enter a valid e-mail address."; 
+			email.focus(); 
+			flag=1;
+		} 
+
+		else if (email.value.indexOf(".", 0) < 0)				 
+		{ 
+			value1+="Please enter a valid e-mail address.\n"; 
+			email.focus(); 
+			flag=1;
+		} 
+
+		if (password.value == "")					 
+		{ 
+			value1+="Please enter your password\n"; 
+			password.focus(); 
+			flag=1; 
+		} 
+	}
+	
+
+	if(flag==0){
+		func();
+	}
+	else{
+		alert(value1);
+	}
+	
+}
+
